f40df86ad62a698fb91e64aafaf4467b99b7c772
GaboDotCodes <gabedu2000@gmail.com>
2022-05-07 18:21:09 -0500

    get images

--- src/js/helpers/getImages.js
+++ src/js/helpers/getImages.js
@@ -2,7 +2,7 @@ import { createClient } from 'pexels';
 
 const client = createClient('563492ad6f9170000100000178405c376a004403b407dc7646fb95de');
 
-const getImages = async (query, page = 1, per_page = 3) => client.photos.search({ query, per_page, page })
+const getImages = async (query, page = 1, per_page = 20) => client.photos.search({ query, per_page, page })
 
 export default getImages;
 
