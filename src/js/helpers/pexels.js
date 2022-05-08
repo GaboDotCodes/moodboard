d4abf1e0088e12ea43f99af89258f2d1fc8de33b
GaboDotCodes <gabedu2000@gmail.com>
2022-05-08 18:08:46 -0500

    Pexels

--- src/js/helpers/pexels.js
+++ src/js/helpers/pexels.js
@@ -4,5 +4,10 @@ const client = createClient('563492ad6f9170000100000178405c376a004403b407dc7646f
 
 const getImages = async (query, page = 1, per_page = 20) => client.photos.search({ query, per_page, page })
 
-export default getImages;
+const getImage = async (id) => client.photos.show({ id })
+
+export {
+  getImages,
+  getImage,
+};
 
