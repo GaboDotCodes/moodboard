18d2a67c62d0a16fb62f607af0524a1d45b244a0
GaboDotCodes <gabedu2000@gmail.com>
2022-05-08 20:06:45 -0500

    DOMContentLoaded

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,12 @@ import Router from "./Router/Router";
 import routes from "./Router/routes";
 import state from "./State/state";
 
-const container = document.querySelector('#root');
+window.addEventListener('DOMContentLoaded', async () => {
+  const container = document.querySelector('#root');
+  state.dispatch('router.set', new Router(container, routes));
+  const { router } = state.getState()
+  router.load(routes.home)
+})
 
-state.dispatch('router.set', new Router(container, routes));
 
-const { router } = state.getState()
 
-router.load(routes.home)
