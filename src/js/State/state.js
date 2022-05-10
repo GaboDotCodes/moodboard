5e109c2875af875388f326a00e50cff24bfff626
GaboDotCodes <gabedu2000@gmail.com>
2022-05-10 17:35:07 -0500

    Init state

--- src/js/State/state.js
+++ src/js/State/state.js
@@ -1,6 +1,6 @@
 import stateActions from "./stateActions/stateActions";
 import StateManager from "./StateManager";
 
-const state = new StateManager(stateActions, {})
+const state = new StateManager('moodboard', stateActions, {})
 
 export default state;
