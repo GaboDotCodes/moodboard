451cb23d0931afb8e09d0773ef0fe9c8072e6c32
GaboDotCodes <gabedu2000@gmail.com>
2022-05-17 20:52:17 -0500

    add profile to routes

--- src/js/Router/routes.js
+++ src/js/Router/routes.js
@@ -3,6 +3,7 @@ import Route from "./Route";
 const routes = {
   home: new Route('home', 'view-home'),
   error: new Route('error', 'view-error'),
+  profile: new Route('profile', 'view-profile'),
 };
 
 export default routes;
