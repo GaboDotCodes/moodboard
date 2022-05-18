c68d129c98970945f5c9c30b9938008a2f4520b0
GaboDotCodes <gabedu2000@gmail.com>
2022-05-17 20:54:42 -0500

    add routes to StateActions

--- src/js/State/stateActions/stateActions.js
+++ src/js/State/stateActions/stateActions.js
@@ -1,12 +1,14 @@
 import favoritesActions from "./favoritesActions";
 import queryActions from "./queryActions";
 import routerActions from "./routerActions";
+import routesActions from "./routesActions";
 
 favoritesActions
 const stateActions = {
   favorites: favoritesActions,
   query: queryActions,
   router: routerActions,
+  routes: routesActions,
 }
 
 export default stateActions;
