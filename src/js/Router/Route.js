b3c23dd45f2372354230ba81b37fcc5f16c722b5
GaboDotCodes <gabedu2000@gmail.com>
2022-05-17 20:51:41 -0500

    Change error

--- src/js/Router/Route.js
+++ src/js/Router/Route.js
@@ -4,7 +4,7 @@ class Route {
 
   constructor(path, view) {
     if (typeof path !== 'string') throw new Error('Path must be a string');
-    if(!window.customElements.get(view)) throw new Error('View must be a custom element');
+    if(!window.customElements.get(view)) throw new Error(`View ${view} must be a custom element`);
     this.#path = path;
     this.#view = view;
   }
